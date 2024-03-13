600f06044894458a4c3c9590d8ea5c7e421c21db
Artur <artur@intech.ee>
2024-03-13 15:07:33 +0300

    test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,12 +3,11 @@
 if ENV['COVERAGE']
   require 'simplecov'
   require 'simplecov_json_formatter'
-  require 'coveralls'
 
   SimpleCov.formatters = [
     SimpleCov::Formatter::HTMLFormatter,
-    SimpleCov::Formatter::JSONFormatter,
-    Coveralls::SimpleCov::Formatter
+    SimpleCov::Formatter::JSONFormatter
+    # Coveralls::SimpleCov::Formatter
   ]
   SimpleCov.start do
     add_filter '/test/'
